8e0b1742e78ea03d852455296c9f984ad7cde640
Elizabeth Ching <elyching@gmail.com>
2015-04-18 16:28:16 -0700

    Adding more styles and info page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+  get 'pages' => 'pages#index'
+
+  resources :comments
   resources :adventures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'adventures#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
